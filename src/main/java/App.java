98fbe897a1d23d4ec7cfaf40ac3ceb1fba803173
Ivan Puryakov <puryakov@vk.com>
2025-01-27 14:14:21 +0300

    Task done
    Add the Cat class, configure Spring, do a comparative check

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldBean1.getMessage());
+        System.out.println(helloWorldBean2.getMessage());
+        System.out.println("Сравнение bean HelloWorld:" + helloWorldBean1.equals(helloWorldBean2));
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getMessage());
+        System.out.println(catBean2.getMessage());
+        System.out.println("Сравнение bean Cat:" + catBean1.equals(catBean2));
     }
 }
